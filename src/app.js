8f86debe9b63c7de4f1d27326dd88561e9c9b5cd
AnlianaAguilar <anlianaaguilar@gmail.com>
2020-04-08 08:47:47 -0400

    prueba

---
+++ src/app.js
@@ -0,0 +1,9 @@
+//Comentario//
+function mostrarpagina2() {
+    document.getElementById("pagina1").style.display="none";
+    document.getElementById("pagina2").style.display="block";    
+}
+function mostrarpagina3() {
+    document.getElementById("pagina2").style.display="none";
+    document.getElementById("pagina3").style.display="block";    
+}
