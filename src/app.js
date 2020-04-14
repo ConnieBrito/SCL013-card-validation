9c5f2e119e118608d23d136d0843433fa8fbfcd7
ConnieBrito <constanza.brito.28@gmail.com>
2020-04-14 10:14:22 -0400

    last commit

--- src/app.js
+++
@@ -1,9 +0,0 @@
-//Comentario//
-function mostrarpagina2() {
-    document.getElementById("pagina1").style.display="none";
-    document.getElementById("pagina2").style.display="block";    
-}
-function mostrarpagina3() {
-    document.getElementById("pagina2").style.display="none";
-    document.getElementById("pagina3").style.display="block";    
-}
